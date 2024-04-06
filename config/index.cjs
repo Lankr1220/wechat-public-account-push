5caa9e71c3686f865a4b3248f49db407b6868a56
Lankr1220 <166133212+Lankr1220@users.noreply.github.com>
2024-04-06 14:04:35 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -42,14 +42,4 @@ const USER_CONFIG = {
       ],
     },
   ],
-  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'YyI_FVt3f1PZ5U4Oo9MplyT-N0ZnqOZ5Db4fDP0ZrGs',
-  CALLBACK_USERS: [
-    {
-      name: '自己',
-      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ozAkf6xl6L_CH_MGZLMw2OE7kuac',
-    }
-  ],
-}
 module.exports = USER_CONFIG
